cb36f27923cc272df61f5b111b50d9fc57ba5ed4
Omar Ibrahim <omaribrahim@cs.uchicago.edu>
2022-12-15 19:48:16 -0600

    fixing content shift

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -22,7 +22,7 @@ const Sidebar = ({active}) => {
                 <SidebarItem img="\home.png" href="http://www.omar-ibrahim.com" sub="Go Home"/>
                 <SidebarItem img="http://tictactoe.omar-ibrahim.com/favicon.png" href="http://tictactoe.omar-ibrahim.com" sub="Impossible Tic-Tac-Toe" active={active === 'lightbulb'}/>
                 <SidebarItem img="http://lightbulb.omar-ibrahim.com/favicon.png" href="http://lightbulb.omar-ibrahim.com" sub="CSS Lightbulb" active={active === 'light'}/>
-                <SidebarItem img="http://shapes.omar-ibrahim.com/favicon.ico" href="http://shapes.omar-ibrahim.com" sub="Shape Guesser (Beta)" active={active === 'shape'}/>
+                <SidebarItem img="/shape.png" href="http://shapes.omar-ibrahim.com" sub="Shape Guesser (Beta)" active={active === 'shape'}/>
                 <SidebarItem img="/lg.png" href="http://wordle.omar-ibrahim.com" sub="Wordle" active={active === 'wordle'}/>
                 <SidebarItem img="\block.png"href="http://mazebuilder.omar-ibrahim.com" sub="Maze Builder" active={active === 'maze'}/>
                 <SidebarItem img="\ayat.png" href="http://ayat.omar-ibrahim.com" sub="Ayat Search" active={active === 'ayat'}/>
