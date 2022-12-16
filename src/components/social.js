049eb43a2f458c19a601be133a84720dd6dd603f
Omar Ibrahim <omaribrahim@cs.uchicago.edu>
2022-12-16 14:04:22 -0600

    fixing content shift

--- src/components/social.js
+++ src/components/social.js
@@ -3,7 +3,7 @@ const SocialMedia = () => {
     <ul>
         <li>
             <div className="icon">
-                <a href="https://www.facebook.com/omargfh" target="_blank">
+                <a href="https://www.facebook.com/omaribbrahim" target="_blank">
                     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-facebook" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303
                     1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"></path>
@@ -24,7 +24,7 @@ const SocialMedia = () => {
         </li>
         <li>
         <div className="icon">
-            <a href="https://www.instagram.com/omaribbrahim" target="_blank">
+            <a href="https://www.instagram.com/omaribb_" target="_blank">
                 <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-instagram" viewBox="0 0 16 16">
                 <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923
                 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99
